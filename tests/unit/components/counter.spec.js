423cd5c44aa7f37606d92bc2bffb84e4d047c9e4
DavidRivera <0333171040@unjfsc.edu.pe>
2023-05-04 16:07:31 -0500

    test and counter update

---
+++ tests/unit/components/counter.spec.js
@@ -0,0 +1,11 @@
+import { shallowMount } from '@vue/test-utils'
+import Counter from '@/components/Counter'
+
+describe('Counter Component', () => {
+    test('Debe hacer un match con el snapshot', () => {
+
+        const wrapper = shallowMount(Counter)
+
+        expect(wrapper.html()).toMatchSnapshot()
+    })
+ })
